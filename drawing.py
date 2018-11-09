4992ee7c8852311e27d4ef1ec8f073834fa83d1a
S.H. Hayakawa <shuhei@km.phys.sci.osaka-u.ac.jp>
2018-11-09 21:46:42 +0900

    Add Emulsion around target to drawing.py

--- drawing.py
+++ drawing.py
@@ -35,11 +35,10 @@ wdaq = 50
 hdaq = 10
 
 #_______________________________________________________________________________
-def draw_arrow(moveto, width, height, mark=0, color='black'): # mark = 0:both 1:first 2:last 3:none
+def draw_arrow(moveto, width, height, mark=0, color='black', lw=0.1, msize=1.0): # mark = 0:both 1:first 2:last 3:none
   ''' draw arrow '''
   if width == 0 and height == 0: return
-  lw = 0.1 if detector_name == 'DAQ' else 0.1
-  msize = 1.5 if detector_name == 'DAQ' else 1.0
+  if detector_name == 'DAQ': msize = 1.5
   print('0 setgray')
   if color == 'red': print('0.9 0 0 setrgbcolor')
   elif color == 'green': print('0 0.8 0 setrgbcolor')
@@ -663,6 +662,16 @@ def draw_polygon(moveto, array, line_width=0.1, fill_color=1.0):
     print('0.0 setgray {} setlinewidth stroke'.format(line_width))
 
 #_______________________________________________________________________________
+def draw_polygon3d(moveto, w, h, t, lw, fc):
+  ''' draw_polygon3d '''
+  wr = [-w[0], -w[1]]
+  hr = [-h[0], -h[1]]
+  tr = [-t[0], -t[1]]
+  draw_polygon(moveto, [w, h, wr, hr], lw, fc)
+  draw_polygon([moveto[0]+w[0]+h[0], moveto[1]+w[1]+h[1]], [hr, t, h, tr], lw, fc)
+  draw_polygon([moveto[0]+w[0]+h[0], moveto[1]+w[1]+h[1]], [t, wr, tr, w], lw, fc)
+
+#_______________________________________________________________________________
 def draw_square(moveto, width, height, line_width=0.1, fill_color=1.0):
   ''' draw square '''
   if detector_name == 'DAQ':
@@ -696,18 +705,18 @@ def draw():
   print('showpage')
 
 #_______________________________________________________________________________
-def draw_wave(moveto, width, height):
+def draw_wave(moveto, width, height, stroke=True):
   print('newpath {} {} moveto'.format(moveto[0], moveto[1]))
   print('{} {} {} {} {} {} curveto'.format(moveto[0]+width*1/3, moveto[1]+height,
                                            moveto[0]+width*2/3, moveto[1]+height,
                                            moveto[0]+width, moveto[1]))
-  print('gsave 0.1 setlinewidth stroke closepath')
+  print('gsave 0.1 setlinewidth {}'.format('stroke closepath' if stroke else ''))
 
 #_______________________________________________________________________________
-def draw_text(moveto, text, rotate=False, text_size=5):
+def draw_text(moveto, text, rotate=False, text_size=5, font='Times-Roman'):
   ''' draw text '''
   print('0 setgray')
-  print('/Times-Roman findfont {} scalefont setfont'.format(text_size))
+  print('/{} findfont {} scalefont setfont'.format(font, text_size))
   print('{} {} moveto'.format(moveto[0], moveto[1]))
   if rotate:
     print('({}) dup stringwidth pop 0 exch -2 div 0 exch rmoveto'.format(text))
@@ -771,6 +780,7 @@ def draw_logic_and(moveto, size=1.0, etcline=False):
 #_______________________________________________________________________________
 def draw_emulsion():
   global scale
+  full_draw = True
   unit = 1e0
   scale = 0.04*unit
   width = 10*345*scale
@@ -778,31 +788,33 @@ def draw_emulsion():
   height = 40
   thin_gel = 100*scale/unit
   thin_base = 180*scale/unit
+  thin_full = thin_gel * 2 + thin_base
   thick_gel = 480*scale/unit # designed to be 450
   thick_base = 40*scale/unit
+  thick_full = thick_gel * 2 + thick_base
   space = 0
-  zigzag = 2
+  zigzag = 0 if full_draw else 2
   x = 10
   y = 40
+  ystart = y
   lw = 0.4
+  lw2 = 0.1
   fc = 0.9
+  y2 = y - 0.5*(lw-lw2)
   ''' thin '''
-  draw_square([x, y], thin_gel, height, lw, fc)
-  draw_square([x+thin_gel, y], thin_base, height, lw)
-  draw_square([x+thin_gel+thin_base, y], thin_gel, height, lw, fc)
+  draw_square([x, y], thin_full, height, lw, fc)
+  draw_square([x+thin_gel, y2], thin_base, height, lw2)
   draw_line_with_scale([x, y+height], thin_gel, 5, True)
   draw_line_with_scale([x+thin_gel, y+height], thin_base, 10)
   draw_line_with_scale([x+thin_gel+thin_base, y+height], thin_gel, 5)
   x = x + 2*thin_gel + thin_base + space
   height -= zigzag
-  draw_square([x, y], thick_gel, height, lw, fc)
-  draw_square([x+thick_gel, y], thick_base, height, lw)
-  draw_square([x+thick_gel+thick_base, y], thick_gel, height, lw, fc)
+  draw_square([x, y], thick_full, height, lw, fc)
+  draw_square([x+thick_gel, y2], thick_base, height, lw2)
   height += zigzag
   x = x + 2*thick_gel + thick_base + space
-  draw_square([x, y], thick_gel, height, lw, fc)
-  draw_square([x+thick_gel, y], thick_base, height, lw)
-  draw_square([x+thick_gel+thick_base, y], thick_gel, height, lw, fc)
+  draw_square([x, y], thick_full, height, lw, fc)
+  draw_square([x+thick_gel, y2], thick_base, height, lw2)
   draw_line_with_scale([x, y+height], thick_gel, 5)
   draw_line_with_scale([x+thick_gel, y+height], thick_base, 15)
   draw_line_with_scale([x+thick_gel+thick_base, y+height], thick_gel, 5)
@@ -811,23 +823,117 @@ def draw_emulsion():
   draw_text([x-0.5-space/2, y+height*0.625], '...', False, 12)
   x = x + (2*thick_gel + thick_base + space)/4
   height += zigzag
-  draw_square([x, y], thick_gel, height, lw, fc)
-  draw_square([x+thick_gel, y], thick_base, height, lw)
-  draw_square([x+thick_gel+thick_base, y], thick_gel, height, lw, fc)
+  draw_square([x, y], thick_full, height, lw, fc)
+  draw_square([x+thick_gel, y2], thick_base, height, lw2)
   height -= zigzag
   x = x + 2*thick_gel + thick_base + space
-  draw_square([x, y], thin_gel, height, lw, fc)
-  draw_square([x+thin_gel, y], thin_base, height, lw)
-  draw_square([x+thin_gel+thin_base, y], thin_gel, height, lw, fc)
+  draw_square([x, y], thin_full, height, lw, fc)
+  draw_square([x+thin_gel, y2], thin_base, height, lw2)
   x = x + 2*thin_gel + thin_base
   ''' crip '''
   draw_square([9, y-1], x-8, height/4, 0, 1)
   ''' scale '''
-  draw_line_with_scale([10, y+height+20], x-10, 5)
-  draw_text([10+thin_gel+thin_base/2, y+height/4-8], 'thin plate')
-  draw_text([(10+x)/2, y+height/4-8], '11 thick plates')
-  draw_text([x-thin_gel-thin_base/2, y+height/4-8], 'thin plate')
+  draw_text([10+thin_gel+thin_base/2, y+height/4-8], 'thin-type sheet')
+  draw_text([(10+x)/2, y+height/4-8], '11 thick-type sheets')
+  draw_text([x-thin_gel-thin_base/2, y+height/4-8], 'thin-type sheet')
+  draw_text([x-thin_gel-thin_base/2, y+height+4], 'base')
+  draw_arrow([x-thin_gel-thin_base/2, y+height+2], 0, -4, 3)
+  draw_text([x-thin_full-thick_full+thick_gel/2, y+height+4], 'emulsion layer')
+  draw_arrow([x-thin_full-thick_full+thick_gel/2, y+height+2], 0, -4, 3)
   draw_text([x+15, y], '[um]')
+  width = x - 10
+  if not full_draw:
+    draw_line_with_scale([10, y+height+20], width, 5)
+    return
+  ''' entire '''
+  scale = 1.0
+  x = 10
+  y = ystart + height + 30
+  print('[1 1] 0 setdash')
+  draw_polygon3d([x, y+25], [60*scale, -10*scale], [0, 70*scale], [10*scale, 5*scale], 0.1, fc)
+  print('[] 0 setdash')
+  draw_arrow([x, y+25], 60*scale, -10*scale, 3)
+  draw_arrow([x+60*scale, y+25-10*scale], 0, 70*scale, 3)
+  draw_arrow([x+60*scale, y+25-10*scale], 10*scale, 5*scale, 3)
+  draw_arrow([x+70*scale, y+25-5*scale], 0, 70*scale, 3)
+  print('[1 1] 0 setdash')
+  #draw_arrow([x, y+25], 0, 70*scale, 3, '', 0.4)
+  draw_polygon3d([x+25, y+47], [27*scale, -4.5*scale], [0, 26*scale], [0.4*scale, 0.2*scale], 0.1, 0.75)
+  print('[] 0 setdash')
+  draw_polygon3d([x+15, y+42], [27*scale, -4.5*scale], [0, 26*scale], [0.4*scale, 0.2*scale], 0.1, 0.75)
+  draw_polygon3d([x+13, y+45], [18*scale, -3*scale], [0, 10*scale], [7*scale, 3.5*scale], 0.2, 0.5)
+  draw_text([x+5, y+46], 'K', False, 6, 'Times-Italic')
+  draw_text([x+5+3.5, y+48], '-', False, 7, 'Times-Italic')
+  draw_arrow([x+23, y+36], 0, 10, 3)
+  draw_text([x+23, y+32], 'Diamond target')
+  draw_arrow([x+34, y+17], 4, 4, 3)
+  draw_text([x+16, y+14], 'Emulsion module')
+  draw_arrow([x+18, y+64], -4, 8, 3)
+  draw_text([x+13, y+80], 'Upstream')
+  draw_text([x+13, y+74], 'SSD')
+  draw_arrow([x+38, y+69], 2, 4, 3)
+  draw_text([x+42, y+81], 'Downstream')
+  draw_text([x+42, y+75], 'SSD')
+  draw_arrow([18, y+43], 12, 6, 2, '', 0.8, 3)
+  # nwave = int(60/10)
+  # for i in range(nwave):
+  #   moveto = [x+60/nwave*i, y+95-i*10/nwave]
+  #   wavew = 10
+  #   waveh = 3*(i%2-0.5)
+  #   for j in range(2):
+  #     print('newpath {} {} moveto'.format(moveto[0], moveto[1]))
+  #     print('{} {} {} {} {} {} curveto'.format(moveto[0]+wavew*1/3, moveto[1]+waveh-10/nwave*1/3,
+  #                                              moveto[0]+wavew*2/3, moveto[1]+waveh-10/nwave*2/3,
+  #                                              moveto[0]+wavew, moveto[1]-10/nwave))
+  #     print('{} {} rmoveto'.format(-20*(i%2-0.5), -10*(i%2-0.5)))
+  #     print('0.1 setlinewidth closepath')
+  #   print('stroke')
+  ''' circle '''
+  scale = 2.0
+  x = 10 + 3.7/7*width
+  y = ystart + height + 35
+  r = 50
+  center = [x+r, y+r]
+  dw = 30*scale
+  ssdw1 = 1.8*scale
+  ssdw2 = 2.3*scale
+  air1 = 1.6*scale
+  emw = 11.76*scale
+  air2 = 23.8*scale - air1 - emw
+  print('newpath {} {} moveto'.format(center[0]+r, center[1]))
+  print('{} {} {} 0 360 arc clip'.format(center[0], center[1], r))
+  draw_square([x-dw/2, y+r-dw/2], dw, dw, lw, 0.5)
+  x = x + dw/2
+  draw_square([x, 0], ssdw1, 300, lw, 0.75)
+  draw_square([x+ssdw1+air1, 0], emw, 300, lw, fc)
+  draw_square([x+ssdw1+air1+emw+air2, 0], ssdw2, 300, lw, 0.75)
+  print('initclip')
+  draw_text([x-dw/2, y+r+32], 'Diamond')
+  draw_text([x-dw/2, y+r+27], 'target')
+  draw_text([x+ssdw1+air1+emw/2, y+2*r+8], 'Emulsion')
+  draw_text([x+ssdw1+air1+emw/2, y+2*r+3], 'module')
+  draw_text([x+ssdw1/2-8, y+2*r+3], 'Upstream')
+  draw_text([x+ssdw1/2-8, y+2*r-2], 'SSD')
+  draw_text([x+ssdw1+air1+emw+air2, y+2*r+0], 'Downstream')
+  draw_text([x+ssdw1+air1+emw+air2, y+2*r-5], 'SSD')
+  draw_arrow([x+ssdw1+air1+emw, y-2], 0, -8, 3)
+  draw_arrow([x+ssdw1+air1, y-2], 0, -8, 3)
+  draw_arrow([10, y-10], (x+ssdw1+air1)-10, 0, 3)
+  draw_arrow([10, y-10], 0, -4, 3)
+  draw_arrow([x+ssdw1+air1+emw, y-10], 10+width-(x+ssdw1+air1+emw), 0, 3)
+  draw_arrow([10+width, y-10], 0, -4, 3)
+  x -= dw/2
+  draw_arrow([x-10, y+r], 30, 0, 3, '', lw)
+  draw_arrow([x+20, y+r], 80, 30, 2, '', lw, 2.5)
+  draw_arrow([x+20, y+r], 30, -20, 3, '', lw, 2.5)
+  #draw_circle([x+50, y+r-20], 0.5, 0.1)
+  draw_text([x-10, y+r+2], 'K', False, 6, 'Times-Italic')
+  draw_text([x-10+3.5, y+r+4], '-', False, 7, 'Times-Italic')
+  draw_text([x+95, y+r+32], 'K', False, 6, 'Times-Italic')
+  draw_text([x+95+3.5, y+r+34], '+', False, 4, 'Times-Italic')
+  draw_text([x+50, y+r-27], 'X', False, 6, 'Symbol')
+  draw_text([x+50+3.5, y+r-25], '-', False, 7, 'Times-Italic')
+  draw_circle([center[0], center[1]], r, 0, 0, 360, 0.1)
 
 #_______________________________________________________________________________
 def draw_trigger():
@@ -1186,7 +1292,16 @@ def draw_daq():
 
 #_______________________________________________________________________________
 def draw_matrix():
-  pass
+  xstart = 20
+  ystart = 200
+  ypitch = 24
+  l = 20
+  nseg = [24, 64, 16, 16]
+  for i, name in enumerate(['TOF MT', 'SCH', 'FBH-U', 'FBH-D']):
+    draw_text([xstart, ystart-i*ypitch], name)
+    draw_text([xstart+0.25*l, ystart-i*ypitch-10], nseg[i])
+    draw_arrow([xstart-0.5*l, ystart-5-i*ypitch], l, 0, 2)
+    draw_arrow([xstart-2, ystart-7-i*ypitch], 4, 4, 3)
 
 #_______________________________________________________________________________
 if __name__ == '__main__':
